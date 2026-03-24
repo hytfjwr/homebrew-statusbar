d9f68d18c86095b6eeae951e8dc9f68b8a554c8b
Hayato Fujiwara <133951156+hytfjwr@users.noreply.github.com>
2026-03-25 01:14:50 +0900

    statusbar v0.8.1 (#30)
    Update `statusbar` cask to v0.8.1.
    
    - **URL:**
    `https://github.com/hytfjwr/StatusBar/releases/download/v0.8.1/StatusBar.zip`
    - **SHA256:**
    `c93fd7f7ce0906168bc55570a4f3cea450d9c5e0811149e43a95604bb75d11cb`
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- Casks/statusbar.rb
+++ Casks/statusbar.rb
@@ -1,6 +1,6 @@
 cask "statusbar" do
-  version "0.8.0"
-  sha256 "3fa85a176bd3b8463fa5908449d93877e04f3263e1fa4de98c4baa09e3a5737d"
+  version "0.8.1"
+  sha256 "c93fd7f7ce0906168bc55570a4f3cea450d9c5e0811149e43a95604bb75d11cb"
 
   url "https://github.com/hytfjwr/StatusBar/releases/download/v#{version}/StatusBar.zip"
   name "StatusBar"
